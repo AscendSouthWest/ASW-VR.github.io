46f65c3c48ff03356e94830ada22d47e30353f6b
AscendSouthWest <84541540+AscendSouthWest@users.noreply.github.com>
2021-06-11 02:44:05 -0700

    CT21-027-Configurator

---
+++ XR/CT_VB_21-027.1341.%d.png/keyshot-configurator-imagemapping.js
@@ -0,0 +1,52 @@
+imagemappingdata =
+[
+    {
+        "image": "CT_VB_21-027.1341.1.png",
+        "modelsets": [
+            575259531
+        ],
+        "studio": 575311007
+    },
+    {
+        "image": "CT_VB_21-027.1341.2.png",
+        "modelsets": [
+            575259531
+        ],
+        "studio": 575311006
+    },
+    {
+        "image": "CT_VB_21-027.1341.3.png",
+        "modelsets": [
+            575259531
+        ],
+        "studio": 575311003
+    },
+    {
+        "image": "CT_VB_21-027.1341.4.png",
+        "modelsets": [
+            575259531
+        ],
+        "studio": 575311009
+    },
+    {
+        "image": "CT_VB_21-027.1341.5.png",
+        "modelsets": [
+            575259531
+        ],
+        "studio": 575311008
+    },
+    {
+        "image": "CT_VB_21-027.1341.6.png",
+        "modelsets": [
+            575259531
+        ],
+        "studio": 575311005
+    },
+    {
+        "image": "CT_VB_21-027.1341.7.png",
+        "modelsets": [
+            575259531
+        ],
+        "studio": 575311004
+    }
+];
---
+++ XR/CT_VB_21-027.1341.%d.png/keyshot-configurator-modeldata.js
@@ -0,0 +1,45 @@
+model =
+{
+    "materialgroups": [],
+    "products": [
+        {
+            "checked": true,
+            "icon": "polenantennas.png",
+            "modelset": 575259531,
+            "name": "PolenAntennas"
+        }
+    ],
+    "studios": [
+        {
+            "icon": "front%20view%20brancheless.png",
+            "id": 575311003,
+            "name": "Front View Brancheless"
+        },
+        {
+            "icon": "front%20view.png",
+            "id": 575311004,
+            "name": "Front View"
+        },
+        {
+            "icon": "top%20view%20branchless.png",
+            "id": 575311005,
+            "name": "Top View Branchless"
+        },
+        {
+            "checked": true,
+            "icon": "top%20view.png",
+            "id": 575311006,
+            "name": "Top View"
+        },
+        {
+            "icon": "isometric%20view.png",
+            "id": 575311008,
+            "name": "Isometric View"
+        },
+        {
+            "icon": "isometric%20view%20branchless.png",
+            "id": 575311009,
+            "name": "Isometric View Branchless"
+        }
+    ]
+};
